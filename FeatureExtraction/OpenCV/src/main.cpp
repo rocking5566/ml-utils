4b0975959f2016e59c51e6697bb6bc4f4dfa5378
rocking5566 <aa155495.cs01g@nctu.edu.tw>
2017-11-14 10:56:09 +0800

    [What] Move feature to the sub folder [Why] Prepare to add another feature implementation

--- FeatureExtraction/OpenCV/src/main.cpp
+++ FeatureExtraction/OpenCV/src/main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
+#include "opencv2/opencv.hpp"
 #include "MLControl.h"
+
+using namespace cv;
 
 int main(int argc, char** argv)
 {
     MLControl::GenerateTrainingData();
-    MLControl::GenerateTestingData();
+    //MLControl::GenerateTestingData();
 
+    //MLControl::CopySVMPredictFalseImage();
+
+
+    //MLControl::GenerateRandomData(0.5, 144, 1000);
+    //MLControl::IterateImages("D:/data/phone talking/positive_right_test_20170703/right_test_02_ULSee", "*.jpg", ePlotHogFeature);
+    //MLControl::IterateImages("D:/data/phone talking/negative_test_RotateMinrror", "*LBP_R.jpg", ePlotHogFeature);
+    //MLControl::IterateImages("D:/data/phone talking/positive_right_test_20170703/right_test_01_Network", "*.jpg", eWriteFileName);
 //     Mat img = imread("test.jpg");
 //     vector<float>descriptors = Feature::GetHogDescriptor_32x64_144(img);
 //     cout << descriptors.size() << endl;
