b862400ed30040b4fde619a9bc4900dd7e17436d
rocking5566 <aa155495.cs01g@nctu.edu.tw>
2017-11-16 17:09:13 +0800

    [What] Scale the feature for training SVM

--- Tool/libSVM/train.bat
+++ Tool/libSVM/train.bat
@@ -1,2 +1,3 @@
-svm-train -s 0 -c 8 -g 1 -b 1 right_ULSee_all_train_hog_144_with_left_flip
+svm-scale -s right_all_train_hog_lbp_321.range right_all_train_hog_lbp_321 > right_all_train_hog_lbp_321_scale
+svm-train -s 0 -c 8 -g 1 -b 1 right_all_train_hog_lbp_321_scale
 pause
